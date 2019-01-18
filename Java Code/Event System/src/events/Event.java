779c951169dc735a2df6ede3f20393d9ff069cac
Ptolemy2002 <32689317+Ptolemy2002@users.noreply.github.com>
2019-01-18 15:49:31 -0500

    Added Blackjack Project

--- Java Code/Event System/src/events/Event.java
+++
@@ -1,9 +0,0 @@
-package events;
-
-public interface Event {
-	/**The event to call after this event is called. May be null.*/
-	public Event getAfterwardEvent();
-	
-	/**The event to call after this event is unexpectantly cancelled. May be null.*/
-	public Event getCancelledEvent();
-}
