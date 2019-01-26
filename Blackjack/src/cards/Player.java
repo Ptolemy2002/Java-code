a02624a1ffbed06fbbd1404c6e1972541a015579
Ptolemy2002 <32689317+Ptolemy2002@users.noreply.github.com>
2019-01-26 17:41:44 -0500

    More small updates

--- Blackjack/src/cards/Player.java
+++ Blackjack/src/cards/Player.java
@@ -1,10 +1,15 @@
 package cards;
 
-public class Player {
+public abstract class Player {
 	
 	private Deck hand;
 	private Double money;
 	private Double bet;
+	private int id;
+	
+	public Player(int id) {
+		this.id = id;
+	}
 	
 	public Player deal(Card card) {
 		this.hand.putCardAtBottom(card);
@@ -19,16 +24,36 @@ public class Player {
 		return this.bet;
 	}
 	
-	public void setBet(Double bet) {
+	public Player setBet(Double bet) {
 		this.bet = bet;
+		return this;
 	}
 	
-	public void pay(Double amount) {
+	public Player addBet(Double bet) {
+		this.bet += bet;
+		return this;
+	}
+	
+	public Player removeBet(Double bet) {
+		this.bet -= bet;
+		return this;
+	}
+	
+	public Player pay(Double amount) {
 		this.money += amount;
+		return this;
 	}
 	
-	public void collect(Double amount) {
+	public Player collect(Double amount) {
 		this.money -= amount;
+		return this;
 	}
+	
+	@Override
+	public String toString() {
+		return "Player " + id;
+	}
+	
+	public abstract void play(CardGame game);
 
 }
